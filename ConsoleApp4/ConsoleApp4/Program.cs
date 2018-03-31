bedf22eab0f3b0a983a6cbd34a7ac5d37d651278
Nurana Rustamova <nurana.rustamova.99@gmail.com>
2018-03-31 22:42:33 +0400

    my first console app

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! My name is Nani");
+            Console.WriteLine("What is your name");
+            string name=Console.ReadLine();
+            Console.WriteLine("My name is {0}", name);
+
+            Console.ReadKey();
+        }
+    }
+}
